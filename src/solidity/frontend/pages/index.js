f775143727a7d9b2c377c0410e54047ab26bae3b
Arturo Vivar <arthas1237@hotmail.com>
2022-05-06 13:43:48 +0200

    first mint proyect done

--- src/solidity/frontend/pages/index.js
+++ src/solidity/frontend/pages/index.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import { useWeb3React } from '@web3-react/core';
-import useRoyaltys from '../hooks/useRoyaltys';
-import './index.css'
-import toast from 'react-hot-toast';
+import React from "react";
+import { useWeb3React } from "@web3-react/core";
+import useRoyaltys from "../hooks/useRoyaltys";
+import "./index.scss";
+import toast from "react-hot-toast";
 
 const MintNft = () => {
   const royaltys = useRoyaltys();
   const { active, account } = useWeb3React();
   const Mint = () => {
     console.log(royaltys);
-    const upc = royaltys.methods.mint().send({
-      from: account
-    })
-    .on('transactionHash', (txHash) => {
-      toast.success(`Minting your Nft song... Transaction:${txHash}`)
-    })
-    .on('receipt', () => {
-      toast.success(`Your Nft Song has been Minted`)
-    })
-    .on('error', (error) => {
-      toast(`An ${error.message} ocurred`)
-    })
+    const upc = royaltys.methods
+      .mint()
+      .send({
+        from: account,
+      })
+      .on("transactionHash", (txHash) => {
+        toast.success(`Minting your Nft song... Transaction:${txHash}`);
+      })
+      .on("receipt", () => {
+        toast.success(`Your Nft Song has been Minted`);
+      })
+      .on("error", (error) => {
+        toast(`An ${error.message} ocurred`);
+      });
   };
 
   return (
     <>
-      <button onClick={() => Mint()}>Mintear NFT</button>
+      <div className="song-nft-container">
+        <div className="card jvliah animated"></div>
+        <div className="mint-button">
+        <button onClick={() => Mint()}>Mintear NFT</button>
+        </div>
+        
+      </div>
     </>
   );
 };
 
-export default MintNft
-;
+export default MintNft;
